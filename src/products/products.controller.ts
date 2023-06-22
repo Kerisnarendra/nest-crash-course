c18f2cbc0c58121d0ae729cc2b0927b2c52a97cb
Kerisnarendra <kakerisn@gmail.com>
2023-06-22 14:01:51 +0800

    overview-exceptions

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Product } from './entities/product.entity';
+import { CustomException } from 'src/exceptions/custom-exception';
 
 @Controller('products')
 export class ProductsController {
@@ -15,5 +16,23 @@ export class ProductsController {
     @Get()
     findAll(): Product[] {
       return this.usersService.findAll();
+    }
+
+    @Get('throw-exception')
+    findWithException() {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN)
+    }
+
+    @Get('throw-overrided-exception')
+    findWithOverridedException() {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'This is a custom message',
+      }, HttpStatus.FORBIDDEN);
+    }
+
+    @Get('throw-custom-exception')
+    findWithCustomException() {
+      throw new CustomException();
     }
 }
